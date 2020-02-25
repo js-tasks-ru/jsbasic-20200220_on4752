1886cd7a86d3a99d97e036622edde5735eab2fbd
Aleksandr Svetlakov <on4752@icloud.com>
2020-02-25 21:13:54 +0700

    created func - getMinMax

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,16 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let arr = [];
+
+    for ( let item of str.split(/,| /) ) {
+        if ( !isNaN( parseFloat(item) ) ) {
+            arr.push( parseFloat(item) );
+        };
+    }
+
+    return {
+        min: Math.min.apply( null, arr ),
+        max: Math.max.apply( null, arr ),
+    }
 }
